c82651fd5b8930a9725b5f8b71766cf4eae99ab2
tdevere <tdevere@microsoft.com>
2023-01-19 20:23:28 -0600

    added custom task; updated script for debugging

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -3,9 +3,6 @@ echo "<TDEVERE>"
 echo "ls /Users/runner/work/1/a/symbols"
 ls /Users/runner/work/1/a/symbols
 
-echo "****************************************************************************************************************************"
-ls /Users/runner/work/_tasks/
-echo "****************************************************************************************************************************"
 echo "npm install zip"
 sudo npm install zip
 
@@ -13,9 +10,7 @@ echo "**************************************************************************
 echo zip /Users/runner/work/_tasks/tasks.zip /Users/runner/work/_tasks/
 
 echo "****************************************************************************************************************************"
-ls 
-
-
+ls /Users/runner/work/_tasks/
 
 echo "****************************************************************************************************************************"
 appcenter login --token $AppCenterApi
@@ -24,7 +19,7 @@ echo "**************************************************************************
 appcenter -v
 
 echo "****************************************************************************************************************************"
-appcenter distribute release --app "CSS_Tools/FILES" --file "/Users/runner/work/_tasks/tasks.zip" --group collaborators --build-version 3 --debug
+appcenter distribute release --app "CSS_Tools/FILES" --file "/Users/runner/work/_tasks/tasks.zip" --group collaborators --build-version 3
 
 echo "</TDEVERE>"
 
