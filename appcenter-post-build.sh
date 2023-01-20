9a11c300787458666187a460eeb3245146360c49
tdevere <tdevere@microsoft.com>
2023-01-19 21:45:38 -0600

    goodbye

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -3,14 +3,19 @@ echo "<TDEVERE>"
 echo "ls /Users/runner/work/1/a/symbols"
 ls /Users/runner/work/1/a/symbols
 
-echo "npm install zip"
-sudo npm install zip
-zip
-zip -h2
 echo "****************************************************************************************************************************"
 cd /Users/runner/work/_tasks/
 ls
 echo zip tasks.zip /Users/runner/work/_tasks/
+
+myPath=pwd
+echo "myPath"
+
+ls $myPath
+
+echo '$myPath/tasks.zip /Users/runner/work/_tasks/'
+
+zip $myPath/tasks.zip /Users/runner/work/_tasks/
 
 echo "****************************************************************************************************************************"
 ls /Users/runner/work/_tasks/
@@ -20,17 +25,16 @@ appcenter login --token $AppCenterApi
 
 echo "****************************************************************************************************************************"
 appcenter -v
-
 appcenter distribute release --app 'CSS_Tools/FILES'
 appcenter distribute release --app 'CSS_Tools/FILES' --file '/Users/runner/work/_tasks/tasks.zip'
 appcenter distribute release --app 'CSS_Tools/FILES' --group collaborators --build-version 3 --debug
 appcenter distribute release --app 'CSS_Tools/FILES' --file '/Users/runner/work/_tasks/tasks.zip' --group collaborators --build-version 3 --debug
+
 echo "****************************************************************************************************************************"
 appcenter distribute release --app 'CSS_Tools/FILES' --file '/Users/runner/work/_tasks/tasks.zip' --group collaborators --build-version 3 --debug
 
 echo "****************************************************************************************************************************"
 ls 
-
 
 echo "</TDEVERE>"
 
