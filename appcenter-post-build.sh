ea4b680ecfe7159ef66d642434ae4ed343f6646e
tdevere <tdevere@microsoft.com>
2023-01-19 18:53:16 -0600

    updated script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+echo "<TDEVERE>"
+echo "ls /Users/runner/work/1/a/symbols"
+ls /Users/runner/work/1/a/symbols
+echo "</TDEVERE>"
+
 echo "[Build Configuration]"
 eval cat $HOME/systeminfo.md
