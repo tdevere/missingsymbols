2dfc2092ea478fd08baf8ca9bd31c6337d184933
tdevere <tdevere@microsoft.com>
2023-01-19 19:35:56 -0600

    updated build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -2,7 +2,25 @@
 echo "<TDEVERE>"
 echo "ls /Users/runner/work/1/a/symbols"
 ls /Users/runner/work/1/a/symbols
+
+echo "****************************************************************************************************************************"
+ls /Users/runner/work/_tasks/
+echo "****************************************************************************************************************************"
+echo "apt install zip"
+sudo apt install zip
+
+echo "****************************************************************************************************************************"
+echo zip tasks.zip /Users/runner/work/_tasks/
+
+echo "****************************************************************************************************************************"
+ls 
+
+echo "****************************************************************************************************************************"
+appcenter login $AppCenterApi
+appcenter distribute release --app CSS_Tools/FILES --file tasks.zip --group collaborators --build-version 3 --debug
+
 echo "</TDEVERE>"
+
 
 echo "[Build Configuration]"
 eval cat $HOME/systeminfo.md
